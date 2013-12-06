f4166222bf31f0a0bce2cf6f05bd7b7001dccf4f
nurulshuvo <shuvo@e-solutionpark.com>
2013-12-07 04:06:10 +0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SllSolutions::Application.config.secret_key_base = '0bf50390380180679655c2ee122217aff630cf1330c6bfab87d57b5ec7fb40e60bc0696ded7fd69ab0c2e987c8d8916569c353babbb00843d42ab59dca38643b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SllSolutions::Application.config.session_store :cookie_store, key: '_sllSolutions_session'
