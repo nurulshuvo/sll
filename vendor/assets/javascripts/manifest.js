1368a80d18ec87bb89a246abf70bfe864f720ac5
nurulshuvo <shuvo@e-solutionpark.com>
2013-12-08 03:29:49 +0600

    ui complete

---
+++ vendor/assets/javascripts/manifest.js
@@ -0,0 +1,6 @@
+//=require revolution-slider
+//=require layerslider.kreaturamedia.jquery
+//=require layerslider.transitions
+//= require jquery-transit-modified
+//= require jquery-easing-1.3
+//= require_tree .
